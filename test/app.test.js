c5ad34d4d12c9d7d8e7ff83e903cc02e867599a9
Maya Bowman <mayashady1@gmail.com>
2019-10-13 16:21:36 -0400

    worked on testing

--- test/app.test.js
+++ test/app.test.js
@@ -16,5 +16,36 @@ describe('GET /apps', () => {
         );
       });
   });
-})
+
+  it('should be 400 if sort is incorrect', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'invalid' })
+      .expect(400, 'Sort must be one of rating or app')
+  });
+
+  it('should sort by rating', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'rating' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array')
+        let sorted = true;
+
+        let i = 0;
+        while(i < res.body.length -1) {
+          const appAtI = res.body[i];
+          const appAtIPlus1 = res.body[i + 1];
+          if(appAtIPlus1.Rating < appAtI.Rating) {
+            sorted = false;
+            break;
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+});
 
