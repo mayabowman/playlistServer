d183e6d8d2615915ee1f0a23907e314b0bcee40e
Maya Bowman <mayashady1@gmail.com>
2019-10-13 15:48:37 -0400

    adding testing

--- test/app.test.js
+++ test/app.test.js
@@ -7,16 +7,14 @@ describe('GET /apps', () => {
     return supertest(app)
       .get('/apps')
       .expect(200)
-      .expect(res => expect(res.body).to.be.an('array'));
-      
+      .then(res => {
+        expect(res.body).to.be.an('array')
+        expect(res.body).to.have.lengthOf.at.least(1);
+        const theApp = res.body[0];
+        expect(theApp).to.include.all.keys(
+          'App', 'Category', 'Rating', 'Reviews', 'Size', 'Installs', 'Type', 'Price', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver', 'Android Ver'
+        );
+      });
   });
 })
 
-// .then(res => {
-//   expect(res.body).to.be.an('array')
-//   expect(res.body).to.have.lengthOf.at.least(1);
-//   const theApp = res.body[0];
-//   expect(theApp).to.include.all.keys(
-//     'App', 'Category', 'Rating', 'Reviews', 'Size', 'Installs', 'Type', 'Price', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver', 'Android Ver'
-//   );
-// });
